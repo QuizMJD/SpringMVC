12ce393e6e6b1ca83e5f34d9a3adf93cb3e0685a
phamqui <darkscopio2@gmail.com>
2024-09-16 11:58:48 +0700

    #81.1

--- src/main/java/vn/hoidanit/laptopshop/domain/OrderDetail.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/OrderDetail.java
@@ -19,5 +19,43 @@ public class OrderDetail {
     @JoinColumn(name = "product_id")
     private Product product;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
 }
